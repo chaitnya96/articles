7f11f88dee224f7254a9646437f25d5391039a69
chaitnya96 <chaitnyaparikh@gmail.com>
2018-10-23 23:25:44 +0530

    change permission

--- articlesApp/api/views.py
+++ articlesApp/api/views.py
@@ -1,12 +1,13 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny
 from articlesApp.models import Articles
 from .serializers import ArticleSerializers
-from rest_framework.viewsets import ModelViewSet
 
 
 class ArticleListView(ListCreateAPIView):
     queryset = Articles.objects.all()
     serializer_class = ArticleSerializers
+    permission_classes = AllowAny
 
 
 class ArticleDetailView(RetrieveUpdateDestroyAPIView):
